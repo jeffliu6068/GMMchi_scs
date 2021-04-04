22fcc9457f62bf60613a3cba2dbe4b2eb57c6d2e
Jeff Liu <50810835+jeffliu6068@users.noreply.github.com>
2021-04-04 02:59:20 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='GMMchi_scs',
+    version='0.1',
+    author='Jeff Liu',
+    author_email='jeffliu6068@gmail.com',
+    packages=['GMMchi_scs'],
+    url='https://github.com/jeffliu6068/GMMchi_single_cell_pipeline',
+    license='LICENSE.txt',
+    description='Pre-print version of the GMMchi-based single cell postprocessing pipeline',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'pandas',
+        'scipy',
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'sklearn',
+        'tqdm',
+        'umap-learn',
+        'scprep'
+    ],
+)
