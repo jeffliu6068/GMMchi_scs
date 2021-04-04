25a0454168f0b346b863f21846ed2ad71a7c106d
Jeff Liu <50810835+jeffliu6068@users.noreply.github.com>
2021-04-04 04:02:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='GMMchi_scs',
+    name='GMMchi_scs_pipeline',
     version='0.1',
     author='Jeff Liu',
     author_email='jeffliu6068@gmail.com',
-    packages=['GMM_scs_pipeline'],
-    url='https://github.com/jeffliu6068/GMMchi_scs',
+    packages=['GMMchi_scs_pipeline'],
+    url='https://github.com/jeffliu6068/GMMchi_scs_pipeline',
     license='LICENSE.txt',
     description='Pre-print version of the GMMchi-based single cell postprocessing pipeline',
     long_description=open('README.md').read(),
